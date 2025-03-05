20495c2014d5d9b3896b34a74463fe8e1c951b2a
mpsalunggg <putrasatria893@gmail.com>
2025-03-05 12:03:07 +0700

    feat: add doctype event activity

---
+++ ticketing_event/ticketing_event/doctype/event_activity/event_activity.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, fe and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class EventActivity(Document):
+	pass
