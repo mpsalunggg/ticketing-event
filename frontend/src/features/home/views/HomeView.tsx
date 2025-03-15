0abd95a53f97c40e087577839ba9b1827cd6afa0
mpsalunggg <putrasatria893@gmail.com>
2025-03-15 12:31:11 +0700

    chore: adjust responsife card

--- frontend/src/features/home/views/HomeView.tsx
+++ frontend/src/features/home/views/HomeView.tsx
@@ -1,60 +1,54 @@
 import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
 import CheckoutTicket from "@/features/home/components/CheckoutTicket";
 import { useHome } from "@/features/home/hooks";
 import { useModalStore } from "@/stores";
-import moment from "moment";
-import { Link, useNavigate } from "react-router-dom";
-import { useFrappeAuth } from "frappe-react-sdk";
 
 const HomeView = () => {
-   const { dataEventActivity, navigate } = useHome();
+   const { dataEventActivity } = useHome();
    const { openModal, modalType, data } = useModalStore();
+
    return (
-      <div className="flex flex-col gap-y-6">
-         <div className="grid grid-cols-4 gap-6">
-            <Link to={"/home/test"}>test</Link>
-            <button onClick={() => navigate("/home/test")}>testt</button>
+      <div className="flex flex-col gap-y-6 px-4">
+         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6">
             {dataEventActivity.map((item, i) => (
-               <>
-                  <Card key={i} className="p-3 shadow-sm">
-                     <div className="relative">
-                        <img
-                           src={item.poster}
-                           alt=""
-                           className="border rounded-md w-full"
-                        />
-                        <div className="absolute top-2 right-2">
-                           {!item.total_ticket && (
-                              <Badge variant="destructive">Sold Out</Badge>
-                           )}
-                        </div>
+               <Card key={i} className="p-3 shadow-sm">
+                  <div className="relative">
+                     <img
+                        src={item.poster}
+                        alt=""
+                        className="border rounded-md w-full"
+                     />
+                     <div className="absolute top-2 right-2">
+                        {!item.total_ticket && (
+                           <Badge variant="destructive">Sold Out</Badge>
+                        )}
                      </div>
-                     <div className="flex flex-col gap-1">
-                        <p className="text-xl font-semibold">{item.title}</p>
-                        <p>Rp. {item.price}</p>
-                     </div>
-                     <Button
-                        disabled={!item.total_ticket}
-                        onClick={() =>
-                           openModal({
-                              type: "co",
-                              data: {
-                                 price: item.price,
-                                 title: item.title,
-                                 image: item.poster,
-                                 venue: item.venue,
-                                 total_ticket: item.total_ticket,
-                              },
-                           })
-                        }
-                     >
-                        Buy Tickets
-                     </Button>
-                  </Card>
-               </>
+                  </div>
+                  <div className="flex flex-col gap-1">
+                     <p className="text-xl font-semibold">{item.title}</p>
+                     <p>Rp. {item.price}</p>
+                  </div>
+                  <Button
+                     className="w-full mt-2"
+                     disabled={!item.total_ticket}
+                     onClick={() =>
+                        openModal({
+                           type: "co",
+                           data: {
+                              price: item.price,
+                              title: item.title,
+                              image: item.poster,
+                              venue: item.venue,
+                              total_ticket: item.total_ticket,
+                           },
+                        })
+                     }
+                  >
+                     Buy Tickets
+                  </Button>
+               </Card>
             ))}
          </div>
 
@@ -67,18 +61,6 @@ const HomeView = () => {
                totalTicket={data.total_ticket}
             />
          )}
-
-         {/* <Card className="p-0">
-            <div className="flex flex-col gap-y-6 py-6 px-8">
-               <div>
-                  <DataTable
-                     columns={ColumnsHome}
-                     data={dataEventActivity}
-                     isToolbar={false}
-                  />
-               </div>
-            </div>
-         </Card> */}
       </div>
    );
 };
